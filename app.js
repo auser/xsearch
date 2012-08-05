f6d5c7380509b70c6d3a1aff1b97e4950a7f79a9
Xavier Damman <xdamman@gmail.com>
2012-08-04 19:27:17 -0700

    putting things together

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+require('./routes')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
+var SearchController = require('../lib/search_controller.js');
 
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+module.exports = function(app){
+
+  var sc = new SearchController();
+
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+  app.get('/search.json', function(req, res, next) {
+    var q = req.param('q');
+    sc.search(q);
+    res.send({result:'ok'});
+  });
+
 };
